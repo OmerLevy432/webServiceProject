abf716bfbe3e1f2a0068a3cb28385fedd8ac8f31
unknown <olevy34@gmail.com>
2025-10-22 00:03:29 +0300

    started working on orders class

---
+++ webService/webService/App_Code/Orders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace webService.App_Code
+{
+    public class Orders: IDbAction
+    {
+        public int orderId { get; private set; }
+        public int userId { get; set; }
+
+    }
+}
