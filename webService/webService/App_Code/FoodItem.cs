b8ae4b3e6fcb48f78221ef9204de4b060578e964
unknown <olevy34@gmail.com>
2025-10-21 22:00:39 +0300

    finished 2 table classes

---
+++ webService/webService/App_Code/FoodItem.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace webService.App_Code
+{
+    public class FoodItem : IDbAction
+    {
+        // define getters and setters
+        public int itemId { get; }
+        public double itemPrice { get; set; }
+        public string itemDescription { get; set; }
+        public int userId { get; set; }
+
+        // constructors
+        public FoodItem() { }
+        public FoodItem(int itemId)
+        {
+            this.itemId = itemId;
+            this.Init();
+        }
+
+        // inits the food item from database data
+        public int Init()
+        {
+            // create string query and execute it
+            string query = string.Format("select * from foodItems where itemId={0}", this.itemId);
+            DataSet itemsTable = DbQ.ExecuteQuery(query);
+
+            // check if there are items within the dataset
+            if (itemsTable.Tables[0].Rows.Count > 0)
+            {
+                this.itemDescription = itemsTable.Tables[0].Rows[0]["itemDescription"].ToString();
+                this.itemPrice = double.Parse(itemsTable.Tables[0].Rows[0]["itemPrice"].ToString());
+                this.userId = int.Parse(itemsTable.Tables[0].Rows[0]["userId"].ToString());
+
+                return 1;
+            }
+
+            // return -1 for no items
+            return -1;
+        }
+
+        // add new item to the database
+        public int AddNew()
+        {
+            string query = string.Format("insert into foodItems (itemPrice, itemDescription, userId) values ('{0}','{1}','{2}')", this.itemPrice, this.itemDescription, this.userId);
+            return DbQ.ExecuteNonQuery(query);
+        }
+
+        // upadates item data in the database
+        public int Update()
+        {
+            string query = string.Format("update foodItems set itemPrice='{0}' , itemDescription='{1}' , userId='{2}' where itemId={4};", this.itemPrice, this.itemDescription, this.userId, this.itemId);
+            return DbQ.ExecuteNonQuery(query);
+        }
+
+        // delete the item from the database
+        public int Delete()
+        {
+            string query = string.Format("delete from items where itemId={0}", this.itemId);
+            return DbQ.ExecuteNonQuery(query);
+        }
+
+        // get item by its creator
+        public static List<FoodItem> GetItemByCreator(int userId)
+        {
+            // retrive item from database 
+            string query = string.Format("select userId from foodItems userId='{0}'", userId);
+            DataSet itemsTable = DbQ.ExecuteQuery(query);
+
+            List<FoodItem> foodItemList = new List<FoodItem>;
+            int amountOfItems= itemsTable.Tables[0].Rows.Count;
+            int i = 0;
+            
+            for (i = 0; i < amountOfItems; i++)
+            {
+                int newItemId = int.Parse(itemsTable.Tables[0].Rows[i]["userid"].ToString());
+                foodItemList.Add(new FoodItem(newItemId));
+            }
+            return foodItemList;
+        }
+
+        // get a list of items
+        public static List<User> GetItemsList()
+        {
+            List<FoodItem> foodItemList = new List<FoodItem>();
+            int newItemId = 0;
+            int i = 0;
+            int amountOfItems = 0;
+
+            // retrive the users from the database
+            string query = string.Format("select userId from foodItems");
+            DataSet itemsTable = DbQ.ExecuteQuery(query);
+
+            // if there are users
+            amountOfItems = itemsTable.Tables[0].Rows.Count;
+
+            for (i = 0; i < amountOfItems; i++)
+            {
+                newItemId = int.Parse(itemsTable.Tables[0].Rows[i]["userId"].ToString());
+                foodItemList.Add(new FoodItem(newItemId));
+            }
+
+            return foodItemList;
+        }
+
+    }
+}
--- webService/webService/App_Code/User.cs
+++ webService/webService/App_Code/User.cs
@@ -1,15 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.EnterpriseServices.Internal;
+using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 
 namespace webService.App_Code
 {
-    public class User :tableObjectInterface
+    public class User : IDbAction
     {
-        public int userId { get; set; }
+        // define get and setters
+        public int userId { get; }
         public string userName { get; set; }
         public string userEmail { get; set; }
+        public string userPassword { get; set; }
+        public int roleId { get; set; }
+
+        // define constructors
+        public User() { }
+        public User(int id)
+        {
+            this.userId = id;
+            this.Init();
+        }
+
+        // inits the user from database data
+        public int Init()
+        {
+            // create string query and execute it
+            string query = string.Format("select * from users where userId={0}", this.userId);
+            DataSet usersTable = DbQ.ExecuteQuery(query);
+
+            // check if there are users within the dataset
+            if (usersTable.Tables[0].Rows.Count > 0)
+            {
+                this.userName = usersTable.Tables[0].Rows[0]["userName"].ToString();
+                this.userEmail = usersTable.Tables[0].Rows[0]["userEmail"].ToString();
+                this.userPassword = usersTable.Tables[0].Rows[0]["userPassword"].ToString();
+                this.roleId = int.Parse(usersTable.Tables[0].Rows[0]["roleId"].ToString());
+
+                return 1;
+            }
+
+            // return -1 for no users
+            return -1;
+        }
+
+        // add new user to the database
+        public int AddNew()
+        {
+            string query = string.Format("insert into users (userName, userEmail,, userPassword, roleId) values ('{0}','{1}','{2}','{3}')", this.userName, this.userEmail, this.userPassword, this.roleId);
+            return DbQ.ExecuteNonQuery(query);
+        }
+        
+        // upadates user data in the database
+        public int Update()
+        {
+            string query = string.Format("update users set userName='{0}' , userEmail='{1}' , userPassword='{2}', roleId='{3}' where userId={4};", this.userName, this.userEmail, this.userPassword, this.roleId, this.userId);
+            return DbQ.ExecuteNonQuery(query);
+        }
+        
+        // delete the user from the database
+        public int Delete()
+        {
+            string query = string.Format("delete from user where userId={0}", this.userId);
+            return DbQ.ExecuteNonQuery(query);
+        }
+
+        // get user by email and password
+        public static User GetUserByEmailPassword(string userEmail, string userPassword)
+        {
+            // retrive user from database 
+            string query = string.Format("select userId from users where userEmail='{0}' and userPassword='{1}'", userEmail, userPassword);
+            DataSet userTable = DbQ.ExecuteQuery(query);
+
+            if (userTable.Tables[0].Rows.Count > 0)
+            {
+                int newUserid = int.Parse(userTable.Tables[0].Rows[0]["userid"].ToString());
+                return new User(newUserid);
+            }
+            return null;
+        }
+
+        // get a list of users
+        public static List<User> GetUsersList()
+        {
+            List<User> userList = new List<User>();
+            int newUserId = 0;
+            int i = 0;
+            int amountOfUsers = 0;
+
+            // retrive the users from the database
+            string query = string.Format("select userId from users ");
+            DataSet usersTable = DbQ.ExecuteQuery(query);
+
+            // if there are users
+            amountOfUsers = usersTable.Tables[0].Rows.Count;
+      
+            for (i = 0; i < amountOfUsers; i++)
+            {
+                newUserId = int.Parse(usersTable.Tables[0].Rows[i]["userId"].ToString());
+                userList.Add(new User(newUserId));
+            }
+            
+            return userList;
+        }
+
     }
 }
